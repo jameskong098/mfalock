29fddd63922fd06ed8f887ca5b2354320e825dac
DoubleOhh <114524539+DoubleOhh@users.noreply.github.com>
2025-03-31 11:20:45 -0400

    added random msg generator

--- audio/utils/audio_utils.py
+++ audio/utils/audio_utils.py
@@ -1,6 +1,8 @@
 from vosk import Model, KaldiRecognizer
 import pyaudio
 import json
+import random_utils
+from audio.utils.random_utils import gen_phrase
 
 # Load the Vosk Model (Ensure "vosk_model" folder is in the same directory)
 model = Model("vosk_model")
@@ -23,6 +25,7 @@ stream = audio.open(format=pyaudio.paInt16, channels=1, rate=16000,
 stream.start_stream()
 
 print("Listening... Speak now!")
+phrase = gen_phrase()
 
 try:
     while True:
@@ -32,6 +35,9 @@ try:
             text = result.get("text", "")  # Extract recognized text
             if text:
                 print(f"You said: {text}")
+                if text == phrase:
+
+                    print("Correct... Opening!!!")
 except KeyboardInterrupt:
     print(" Stopping...")
     stream.stop_stream()
---
+++ audio/utils/random_utils.py
@@ -0,0 +1,24 @@
+import random
+
+words = [
+    "elderberry", "kale", "orange", "vanilla", "fig", "quince", "grape", "xigua",
+    "jalapeño", "nectarine", "lemon", "watermelon", "parsley", "zucchini", "almond",
+    "iceberg", "papaya", "garlic", "apple", "tomato", "quinoa", "yam", "mushroom",
+    "radish", "eggplant", "date", "leek", "daikon", "vinegar", "broccoli", "spinach",
+    "raspberry", "tangerine", "xanthan", "zest", "mango", "cherry", "honeydew",
+    "yogurt", "upland", "nutmeg", "kiwi", "wasabi", "ugly", "strawberry", "oregano",
+    "horseradish", "banana", "fennel", "carrot"
+]
+
+def gen_phrase(num_words: int = 3) -> str:
+    return " ".join(random.choices(words, k=num_words))
+
+def add_word(word: str) -> bool:
+    word = word.strip().lower()
+    if word in words:
+        return False
+    words.append(word)
+    return True
+
+def add_words(words: str) -> bool:
+
